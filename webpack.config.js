41da8f866629faefa9b27bdb4a229a2cd0d9cf1d
Ben4 <benymany511@gmail.com>
2026-02-02 18:54:43 +0300

    Fix

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development', 
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
@@ -22,6 +23,10 @@ module.exports = {
     }),
   ],
   devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
     port: 9000,
     open: true,
   },
